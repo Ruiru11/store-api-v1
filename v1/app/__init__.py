c1bff6fe874734a9c2b0ca571f9c10f3b7ea98a0
Ruiru11 <erastusruiru11gmail.com>
2018-10-16 14:24:44 +0300

    update

--- v1/app/__init__.py
+++ v1/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from .config import Config_by_name
+from app.views.products_views import don_item
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(Config_by_name[config_name])
+    app.register_blueprint(don_item)
     return app
