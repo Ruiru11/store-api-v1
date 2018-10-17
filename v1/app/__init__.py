96006da56c4577c4a497c933baae81ef29b41f46
Ruiru11 <erastusruiru11gmail.com>
2018-10-17 11:50:32 +0300

    [finishes #161277640] create an account

--- v1/app/__init__.py
+++ v1/app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from .config import Config_by_name
 from app.views.products_views import don_item
 from app.views.sales_views import don_sale
+from app.views.users_views import don_user
 
 
 def create_app(config_name):
@@ -10,4 +11,5 @@ def create_app(config_name):
     app.config.from_object(Config_by_name[config_name])
     app.register_blueprint(don_item)
     app.register_blueprint(don_sale)
+    app.register_blueprint(don_user)
     return app
---
+++ v1/app/views/users_views.py
@@ -0,0 +1,38 @@
+from flask import Blueprint
+from flask_restful import reqparse
+
+# local imports
+from app.controllers.users_controllers import Users
+
+don_user = Blueprint('users', __name__, url_prefix="/api/v1")
+
+user_insatnce = Users()
+
+
+@don_user.route("/signup", methods=["POST"])
+def create_user():
+    """
+    This function creates a new user. 
+
+    Parameters:
+            username:Name to be used by user once account is created.
+            email:Email to be used for verification.
+            Password: To be used when signin to created account. 
+                        role: This is the role of user can be attendant/admin
+    Returns:
+            Users:Creates a new user. 
+
+
+    """
+    parser = reqparse.RequestParser()
+    parser.add_argument("username", type=str, required=True,
+                        help="field cannot be empty", location="json")
+    parser.add_argument("email", type=str, required=True,
+                        help="field cannot be empty", location="json")
+    parser.add_argument("password", type=str, required=True,
+                        help="field cannot be empty", location="json")
+    parser.add_argument("role", type=str, required=True,
+                        help="fiels cannot be empty", location="json")
+    data = parser.parse_args()
+
+    return user_insatnce.create_user(data)
